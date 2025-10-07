895034d3f600a1ae0ed99b106a22b315b6d4ebb1
Dražan Dizdar <79365793+dd-km@users.noreply.github.com>
2025-10-07 12:45:48 +0200

    koekcije multi_reg funkcije

--- R/multi_reg.R
+++ R/multi_reg.R
@@ -24,28 +24,24 @@ multi_reg <- function(df) {
   library(car)
   library(plotly)
 
-  # Odabir numeričkih varijabli
   num_vars <- names(df)[sapply(df, is.numeric)]
   in_var <- select.list(num_vars, multiple = TRUE, title = "Odaberi nezavisne varijable:")
   de_var <- select.list(num_vars, multiple = FALSE, title = "Odaberi zavisnu varijablu:")
   
-  # Matrice
   X <- as.matrix(df[, in_var, drop = FALSE])
   Y <- as.matrix(df[de_var])
   n <- nrow(X)
   m <- ncol(X) + 1
   B0 <- rep(1, n)
   Xi <- cbind(B0, X)
-  
-  # Koeficijenti
+ 
   XTXi <- t(Xi) %*% Xi
   XTY <- t(Xi) %*% Y
   b <- solve(XTXi) %*% XTY
   Yp <- Xi %*% b
   e <- Y - Yp
   se <- round(sqrt(sum(e^2)/(n-m)), 2)
-  
-  # R^2 i F-test
+
   pss <- sum((Yp - mean(Yp))^2)
   rss <- sum(e^2)
   ro2 <- pss / (pss + rss)
@@ -55,38 +51,43 @@ multi_reg <- function(df) {
   Fval <- (pss / dfp) / (rss / dfe)
   pf_val <- pf(Fval, dfp, dfe, lower.tail = FALSE)
   
-  # Durbin-Watson test
   dw <- durbinWatsonTest(lm(Y ~ X))
   
-  # Standardizacija i korelacije
+  XY <- cbind(X,Y)
   Z <- scale(X)
   K <- scale(Y)
-  Rxy <- cor(cbind(X,Y))
+  ZK <- round(cbind(Z,K), digits = 2)
+  Rxy <- cor(XY)
   R <- cor(X)
-  r1 <- (t(Z) %*% K) / (n - 1)
+  r1 <- (t(Z) %*% K)/(n-1)
   r <- rbind(0, r1)
   beta1 <- solve(R) %*% r1
-  beta <- rbind(0, beta1)
-  S2 <- 1 / diag(solve(R))
-  s2 <- rbind(0, data.frame(S2))
-  S2xy <- 1 / diag(solve(Rxy))
+  beta <- rbind(0,beta1)
+  S2 <- 1/diag(solve(R))
+  s2 <- data.frame(S2)
+  s2<- rbind(0,s2)
+  S2xy <- 1/diag(solve(Rxy))
   Sxy <- sqrt(S2xy)
   Pr <- diag(Sxy) %*% solve(Rxy) %*% diag(Sxy)
-  pr <- rbind(0, data.frame(Pr[1:(m-1), m] * -1))
-  p <- beta * r
-  
-  # Standardne greške i t-vrijednosti
+  pr <- (Pr[,m]*-1)
+  pr <- pr[1:m-1]
+  pr <- data.frame(pr)
+  pr <- rbind(0,pr)
+  p <- beta*r
+  se <- round(sqrt(sum(e*e)/(n-m)), digits = 2)
   wjj <- solve(XTXi)
-  seb <- se * sqrt(diag(wjj))
-  tb <- b / se
-  df_val <- n - m
-  pval <- sapply(1:m, function(a) 2 * pt(abs(tb[a,1]), df_val, lower.tail = FALSE))
-  
-  # Kreiranje tabele rezultata
+  diagwjj <- diag(wjj)
+  seb <- se*sqrt(diagwjj)
+  tb <- abs(b/seb)
+  df <- n-m
+  pval <- c()
+  for (a in 1:m) {pval[a] <- c(2*(pt(abs(tb[a,1]), df, lower.tail = FALSE)))}
+  tb <- data.frame(tb)
+  pval <- data.frame(pval)
   rez <- cbind(b, seb, beta, pr, r, p, s2, tb, pval)
   colnames(rez) <- c("B","SE(B)", "Beta", "Part_R", "R","P", "Tolerance", "t", "p")
-  rownames(rez) <- c("(Intercept)", in_var)
-  
+  print(round(rez, 3))
+    
   # Plotly graf
   fig <- plot_ly(
     data = round(rez[-1,], 2),
